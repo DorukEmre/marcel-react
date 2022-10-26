8f474f0810fbf2a0ffcfdd078b650d247c4f861b
DorukEmre <doruk.emre@outlook.com>
2022-10-26 22:55:45 +0100

    Fixed wrong routes being authenticated

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -49,9 +49,7 @@ exports.postLogin = async (req, res) => {
 
   const foundUser = await User.findOne({ email }).exec()
 
-  // if (!foundUser) return res.sendStatus(401) //Unauthorized
-  if (!foundUser)
-    return res.status(401).json({ message: 'postLogin !foundUser' })
+  if (!foundUser) return res.sendStatus(401) //Unauthorized
 
   // evaluate password
   const match = await bcrypt.compare(password, foundUser.password)
@@ -88,7 +86,7 @@ exports.postLogin = async (req, res) => {
     res.json({ accessToken })
   } else {
     console.log('else res.sendStatus(401)')
-    res.status(401).json({ message: 'else res.sendStatus(401)' })
+    res.sendStatus(401)
   }
 }
 
@@ -99,7 +97,7 @@ exports.handleRefreshToken = async (req, res) => {
   const cookies = req.cookies
   if (!cookies?.jwt) {
     console.log('No cookies')
-    return res.status(401).json({ message: 'No cookies' })
+    return res.sendStatus(401)
   }
   console.log('cookies.jwt', cookies.jwt)
   const refreshToken = cookies.jwt
