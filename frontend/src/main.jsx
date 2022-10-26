712e9e6f5aae25c48dc139ee15e115502a471aa7
DorukEmre <doruk.emre@outlook.com>
2022-10-26 19:42:48 +0100

    Updated axios route for production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+// import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './reset.css'
 import './index.css'
 import { AuthProvider } from './context/AuthProvider'
 
-import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:9191'
+// import axios from 'axios'
+// axios.defaults.baseURL = 'http://localhost:9191'
 
-// if (process.env.NODE_ENV === 'production') {
-//   disableReactDevTools()
-// }
+if (process.env.NODE_ENV === 'production') {
+  //   disableReactDevTools()
+  console.log('p')
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
