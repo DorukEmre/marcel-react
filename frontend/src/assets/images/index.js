b9d2dc5edaaeb0cb86b3ba9210b70abd779b886a
DorukEmre <doruk.emre@outlook.com>
2022-10-27 18:00:57 +0100

    Filled Spot page

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -1,4 +1,6 @@
 import catMainLogo from './cat_logo-802x1024.png'
 import catMediumLogo from './cat_logo-157x200.png'
+import MarcelFileUpload from './marcel-file-upload.svg'
+import SnapACatLogo from './snap-a-cat_logo.webp'
 
-export { catMainLogo, catMediumLogo }
+export { catMainLogo, catMediumLogo, MarcelFileUpload, SnapACatLogo }
--- frontend/src/pages/Spot.jsx
+++ frontend/src/pages/Spot.jsx
@@ -1,22 +1,70 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import useAxiosPrivate from '../hooks/useAxiosPrivate'
-import {
-  closeIcon,
-  commentIcon,
-  likeFalseIcon,
-  likeTrueIcon,
-  sendMessageIcon,
-} from '../assets/icons'
+import { MarcelFileUpload, SnapACatLogo } from '../assets/images'
 
 const Spot = () => {
   return (
-    <article style={{ padding: '100px' }}>
-      <h1>Spot page</h1>
-      <div className="">
-        <Link to="/">Visit Our Homepage</Link>
-      </div>
-    </article>
+    <main id="spot-page">
+      <section className="spot-container">
+        <div className="spot-logo-container">
+          <img src={SnapACatLogo} alt="cat logo" />
+        </div>
+        <section className="form-panel">
+          <h2>Snap a cat</h2>
+          <form
+            action="/posts/createPost"
+            encType="multipart/form-data"
+            method="POST"
+          >
+            <div className="form-group">
+              <label htmlFor="catName">
+                Name of the cat (or your best nickname)
+              </label>
+              <input
+                type="text"
+                id="catName"
+                name="catName"
+                placeholder="Name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="comment">Add a comment</label>
+              <textarea
+                id="comment"
+                name="comment"
+                placeholder="Add a comment"
+              ></textarea>
+            </div>
+
+            <div className="form-group file-upload-wrapper">
+              <div>
+                <label htmlFor="imageUpload">Add a file</label>
+                <button
+                  htmlFor="imageUpload"
+                  id="custom-file-upload"
+                  type="button"
+                >
+                  <img src={MarcelFileUpload} alt="" height="75" width="75" />
+                  <input
+                    type="file"
+                    id="imageUpload"
+                    name="file"
+                    accept="image/*"
+                    tabIndex="-1"
+                  />
+                </button>
+              </div>
+              <output id="file-list" htmlFor="imageUpload"></output>
+            </div>
+
+            <button type="submit" value="Upload">
+              Submit
+            </button>
+          </form>
+        </section>
+      </section>
+    </main>
   )
 }
 
