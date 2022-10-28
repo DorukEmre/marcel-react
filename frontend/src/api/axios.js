ea908f934b9feb8c6cae9ff737238a72b1a62cc3
DorukEmre <doruk.emre@outlook.com>
2022-10-28 23:20:12 +0100

    Update base url for prod

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:9191'
+// const BASE_URL = 'http://localhost:9191'
 // const BASE_URL = 'https://marcel-the-outdoor-cat.onrender.com'
 // const BASE_URL = 'https://long-blue-sawfish-cuff.cyclic.app'
 // const BASE_URL = 'https://marcel-the-outdoor-cat.up.railway.app/'
 // const BASE_URL = 'https://marcel-the-outdoor-cat-react.herokuapp.com/'
+
+const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://marcel-the-outdoor-cat.up.railway.app/'
+    : 'http://localhost:9191'
 
 export default axios.create({
   baseURL: BASE_URL,
