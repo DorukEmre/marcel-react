ebfe635ef9a7caf51298005b5ecca74c9fd8c0f3
DorukEmre <doruk.emre@outlook.com>
2022-10-30 15:26:47 +0000

    Disabled disableReactDevTools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+// import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import './reset.css'
 import './index.css'
 import { AuthProvider } from './context/AuthProvider'
 
-if (process.env.NODE_ENV === 'production') {
-  disableReactDevTools()
-}
+// if (process.env.NODE_ENV === 'production') {
+//   disableReactDevTools()
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
