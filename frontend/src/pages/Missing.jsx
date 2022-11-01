6ebd840cbfdb54a5b738f41d6dbfb6a6a60ed5b7
DorukEmre <doruk.emre@outlook.com>
2022-11-01 15:11:57 +0000

    Added Go back button to Missing page

--- frontend/src/pages/Missing.jsx
+++ frontend/src/pages/Missing.jsx
@@ -1,12 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Missing = () => {
+  const navigate = useNavigate()
   return (
     <article style={{ padding: '100px' }}>
       <h1>Oops!</h1>
       <p>Page Not Found</p>
+      <br />
       <div className="">
-        <Link to="/">Visit Our Homepage</Link>
+        <p>
+          <Link to="/">Visit Our Homepage</Link>
+          <span> or </span>
+          <button className="go-back-button" onClick={() => navigate(-1)}>
+            Go back
+          </button>
+        </p>
       </div>
     </article>
   )
