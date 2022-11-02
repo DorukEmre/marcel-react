15d866ce105614c28d7d3cedcb49b21ec6f74a7e
DorukEmre <doruk.emre@outlook.com>
2022-11-02 13:09:42 +0000

    Tweaked Cors credentials

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,16 +1,20 @@
 const allowedOrigins = require('./allowedOrigins')
 
+// Configuring CORS w/ Dynamic Origin
+//  This function will be passed a string that is the origin (or undefined if the request has no origin), and a callback with the signature callback(error, origin).
 const corsOptions = {
   origin: (origin, callback) => {
     // if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
     if (2 + 2 === 4) {
+      // set origin to true to reflect the request origin, as defined by req.header('Origin')
       callback(null, true)
     } else {
+      // set it to false to disable CORS.
       callback(new Error('Not allowed by CORS'))
     }
   },
 
-  // credentials: true,
+  credentials: true,
   optionsSuccessStatus: 200,
 }
 
