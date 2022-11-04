bbc7d34698ed8b113be9f9bbcfa9c3be9b0cc530
DorukEmre <doruk.emre@outlook.com>
2022-11-04 16:11:06 +0000

    Clean up unused files and routes

--- backend/routes/main.routes.js
+++ backend/routes/main.routes.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const homeController = require('../controllers/home.controllers')
 const groupsController = require('../controllers/groups.controllers')
 const exploreController = require('../controllers/explore.controllers')
 const postsController = require('../controllers/posts.controllers')
-const profileController = require('../controllers/profile.controllers')
 const verifyJWT = require('../middleware/verifyJWT')
 
 router.use(verifyJWT)
@@ -16,23 +14,7 @@ router.get('/groups', groupsController.getGroups)
 
 // //////////////
 
-// router.get('/', homeController.getIndex)
-
-router.get('/demo', homeController.getDemo)
-
 // GET page to explore map of cats in the neighborhood
 router.get('/explore', exploreController.getExplore)
-
-// POST to join new group
-// router.post("/groups", ensureAuth, groupsController.getGroups);
-// DELETE to remove group
-// router.delete("/groups", ensureAuth, groupsController.getGroups);
-
-// GET profile page
-router.get('/profile', profileController.getProfile)
-// PUT update profile page
-// router.put("/profile", ensureAuth, profileController.getProfile);
-// DELETE profile
-// router.delete("/profile", ensureAuth, profileController.getProfile);
 
 module.exports = router
