f4c4bcd8ab82f0a6290ac7d1bd1f5c9499bd02ea
DorukEmre <doruk.emre@outlook.com>
2022-11-04 21:24:30 +0000

    Set BASE URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,13 +5,25 @@ import axios from 'axios'
 // const BASE_URL = 'https://marcel-the-outdoor-cat.up.railway.app/'
 // const BASE_URL = 'https://marcel-the-outdoor-cat-react.herokuapp.com/'
 
-const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://marcel-the-outdoor-cat-react.herokuapp.com/'
-    : 'http://localhost:9191'
+const domain = window.location.hostname
+console.log(domain)
+let serverLoc
+if (domain === 'marcel-the-outdoor-cat-react.herokuapp.com') {
+  console.log('You are using Heroku')
+  serverLoc = 'https://marcel-the-outdoor-cat-react.herokuapp.com/'
+} else if (domain === 'marcel-the-outdoor-cat.onrender.com') {
+  console.log('You are using Render')
+  serverLoc = 'https://marcel.onrender.com'
+} else if (domain === 'marcel-the-outdoor-cat.cyclic.app') {
+  console.log('You are using Cyclic')
+  serverLoc = 'https://marcel-the-outdoor-cat.cyclic.app/'
+} else if (domain === 'marcel-the-outdoor-cat.up.railway.app') {
+  console.log('You are using Railway')
+  serverLoc = 'https://marcel-the-outdoor-cat.up.railway.app/'
+}
 
-console.log('VITE_BASE_URL', import.meta.env.VITE_BASE_URL)
-console.log(window.location.hostname)
+const BASE_URL =
+  process.env.NODE_ENV === 'production' ? serverLoc : 'http://localhost:9191'
 
 export default axios.create({
   baseURL: BASE_URL,
