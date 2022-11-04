4f0cdeb56160b92d0fb4e7f392571c26032d1eb1
DorukEmre <doruk.emre@outlook.com>
2022-11-04 12:59:01 +0000

    reverted

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,12 +7,10 @@ import axios from 'axios'
 
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? process.env.BASE_URL
+    ? 'https://marcel.onrender.com'
     : 'http://localhost:9191'
 
 console.log(process.env.NODE_ENV)
-console.log(process.env.BASE_URL)
-console.log(BASE_URL)
 
 export default axios.create({
   baseURL: BASE_URL,
