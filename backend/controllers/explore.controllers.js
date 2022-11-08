5f05435d8873cdeae11fd6fece6cebfc22714b7d
DorukEmre <doruk.emre@outlook.com>
2022-11-08 13:27:45 +0000

    Map only displays pictures with showLocation on

--- backend/controllers/explore.controllers.js
+++ backend/controllers/explore.controllers.js
@@ -5,9 +5,7 @@ const Post = require('../models/Post.model')
 module.exports = {
   getExplore: async (req, res) => {
     const posts = await Post.find().populate('user').lean()
-    let catsWithLocation = posts.filter(
-      (post) => post.latitude && post.latitude !== 'undefined',
-    )
+    let catsWithLocation = posts.filter((post) => post.showLocation === true)
 
     res.status(200).json({ catsWithLocation })
   },
