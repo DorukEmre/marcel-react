b8eabf8e1c71084178ef02a26ceb8d4b16d63bd9
DorukEmre <doruk.emre@outlook.com>
2022-11-14 22:15:57 +0000

    Added logos and screenshots for home page

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -2,5 +2,22 @@ import catMainLogo from './cat_logo-802x1024.png'
 import catMediumLogo from './cat_logo-157x200.png'
 import MarcelFileUpload from './marcel-file-upload.svg'
 import SnapACatLogo from './snap-a-cat_logo.webp'
+import phoneFeed from './phone-feed.webp'
+import phoneExplore from './phone-explore.webp'
+import phoneSpot from './phone-spot.webp'
+import logoMongoDBSpring from './Logo_MongoDB_RGB_Logo_Spring-Green.svg'
+import logoNodejs from './Logo_nodejs.svg'
+import logoReact from './Logo_React.svg'
 
-export { catMainLogo, catMediumLogo, MarcelFileUpload, SnapACatLogo }
+export {
+  catMainLogo,
+  catMediumLogo,
+  MarcelFileUpload,
+  SnapACatLogo,
+  phoneFeed,
+  phoneExplore,
+  phoneSpot,
+  logoMongoDBSpring,
+  logoNodejs,
+  logoReact,
+}
