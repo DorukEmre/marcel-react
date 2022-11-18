623caa4f8fd2e0d0c275f323f89cd227a20bd417
DorukEmre <doruk.emre@outlook.com>
2022-11-18 15:40:46 +0000

    minor

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -6,6 +6,7 @@ import CatInfoBox from './CatInfoBox'
 const Map = ({ posts, setPosts, center, zoom, currentUserId }) => {
   const [catInfo, setCatInfo] = useState(null)
   console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+  console.log('process.env.NODE_ENV', process.env.VITE_GM_KEY_PROD)
   const URLKey =
     process.env.NODE_ENV === 'production'
       ? import.meta.env.VITE_GM_KEY_PROD
